5af0251c2d5118273d9a125e0a7b393992b2d6a9
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-07-22 11:45:49 -0700

    "all tests passing"

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,20 @@
-var Bee = function() {
+var Bee = function(food) {
+	Grub.call(this, food);
+	this.age = 5;
+	this.color = "yellow";
+	this.job = "keep on growing";
 };
 
+
+ Bee.prototype.eat = function(eat) {
+    Grub.call(this, eat);
+
+ }
+ // call the Grub superclass
+ // set the prototype
+ // set the constructor
+ // an age property that is set to 5
+ // a color property that is set to yellow
+ // a food property that is inherited from grub
+ // an eat method that is inherited from grub
+ // a job property that is set to keep on growing
--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,32 @@
-var ForagerBee = function() {
+var ForagerBee = function(color, food) {
+	Bee.call(this, color);
+	Grub.call(this, food);
+
+	this.age = 10;
+	this.job = "find pollen";
+	this.color = "yellow";
+	this.canFly = true;
+    this.treasureChest = [];
 };
 
+ForagerBee.prototype.eat = function(eat) {
+    Grub.call(this, eat);
+ }
+
+ForagerBee.prototype.forage = function(treasure) {
+    this.treasureChest.push(treasure);
+ }
+
+
+ // Create a ForagerBee class, in pseudoclassical style, with:
+ // call the Bee superclass
+ // set the prototype
+ // set the constructor
+ // an age property that is set to 10
+ // a job property that is set to find pollen
+ // a color property inherited from bee that is set to yellow
+ // a food property that is inherited from grub
+ // an eat method that is inherited from grub
+ // a canFly property that is set true
+ // a treasureChest property that is set to an empty array []
+ // a forage method that allows the bee to add a treasure to the treasureChest
